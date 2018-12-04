9360c282bbcd0f2919f024d07fd808241bd51cdc
chance <chance.a.cyphers@gmail.com>
2018-12-04 13:48:54 -0500

    Add redux

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {connect} from 'react-redux'
 
-class App extends Component {
-  render() {
+let App = ({msg}) => {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    {msg}
+                </p>
+            </header>
+        </div>
     );
-  }
+};
+
+const mapStateToProps = state => {
+    return {
+        msg: state.message
+    }
 }
+
+App = connect(mapStateToProps)(App);
 
 export default App;
---
+++ src/reducers/PlaceholderReducer.js
@@ -0,0 +1,9 @@
+
+
+const initialState = { message: "hello world" };
+
+function placeholder(state = initialState, action) {
+    return state;
+}
+
+export default placeholder;
